a8d2d65f6dd89dae5e3ae5c9c5ec458e8b4743cb
truar <thibault.ruaro@gmail.com>
2018-12-15 20:27:41 +0100

    Add the script to force my repo commit frequently

---
+++ forceResetPeriodically.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function gitResetHard {
+	git reset --hard HEAD
+}
+
+function loopInfinitly {
+	while :; do
+		sleep $1
+		gitResetHard
+	done
+}
+
+numberOfMinutes=$1
+
+if [ "$numberOfMinutes" = "" ]
+then
+	numberOfMinutes=2
+fi
+numberOfSeconds=$(($numberOfMinutes * 60))
+
+loopInfinitly $numberOfSeconds
