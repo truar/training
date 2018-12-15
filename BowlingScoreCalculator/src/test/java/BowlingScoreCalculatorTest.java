b76b5ba4d2b547ce7cb624822fae34360f5d7e73
truar <thibault.ruaro@gmail.com>
2018-12-15 20:51:59 +0100

    first test ok

--- BowlingScoreCalculator/src/test/java/BowlingScoreCalculatorTest.java
+++ BowlingScoreCalculator/src/test/java/BowlingScoreCalculatorTest.java
@@ -1,3 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class BowlingScoreCalculatorTest {
-    
+
+    @Test
+    public void shouldReturnTheSumForTheFrame() {
+        Assert.assertEquals(1, 1);
+    }
 }
