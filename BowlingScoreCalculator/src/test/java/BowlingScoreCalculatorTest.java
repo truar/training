e5bfa585e8865e834b1360e6d295fe07f19399fc
truar <thibault.ruaro@gmail.com>
2018-12-16 10:09:44 +0100

    Start over. First classic test

--- BowlingScoreCalculator/src/test/java/BowlingScoreCalculatorTest.java
+++ BowlingScoreCalculator/src/test/java/BowlingScoreCalculatorTest.java
@@ -1,7 +1,11 @@
+import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class BowlingScoreCalculatorTest {
 
-    
-
+    @Test
+    public void shouldReturnTheRollScore() {
+        assertThat(1).isEqualTo(1);
+    }
 }
