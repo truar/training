f769798fb246ec793d8d98be7700e92a53bebf17
truar <thibault.ruaro@gmail.com>
2018-12-16 16:30:24 +0100

    Refacto

--- BowlingScoreCalculator/src/test/java/com/zenika/zenikatas/BowlingScoreCalculatorTest.java
+++ BowlingScoreCalculator/src/test/java/com/zenika/zenikatas/BowlingScoreCalculatorTest.java
@@ -12,7 +12,7 @@ public class BowlingScoreCalculatorTest {
     private static final List<Integer> ONLY_STRIKE = new ArrayList<>();
     private static final List<Integer> NO_STRIKE_NOR_SPARE = new ArrayList<>();
     private static final List<Integer> ONLY_SPARE = new ArrayList<>();
-    private static final int MAX_FRAMES_PER_GAME = 10;
+    public static final int MAX_FRAMES_PER_GAME = 10;
 
     static {
         for (int i = 0; i < 12; i++) {
@@ -31,66 +31,22 @@ public class BowlingScoreCalculatorTest {
 
     @Test
     public void shouldReturn300GivenOnlyStrike() {
-        assertThat(calculateScore(ONLY_STRIKE)).isEqualTo(300);
+        assertThat(BowlingScoreCalculator.calculateScore(ONLY_STRIKE)).isEqualTo(300);
     }
 
     @Test
     public void shouldReturn150GivenOnlySpare() {
-        assertThat(calculateScore(ONLY_SPARE)).isEqualTo(150);
+        assertThat(BowlingScoreCalculator.calculateScore(ONLY_SPARE)).isEqualTo(150);
     }
 
     @Test
     public void shouldReturn90GivenOnlySpare() {
-        assertThat(calculateScore(NO_STRIKE_NOR_SPARE)).isEqualTo(90);
-    }
-
-    private static int calculateScore(List<Integer> onlyStrike) {
-        List<Frame> frames = determineFramesFormRolls(onlyStrike);
-        int score = 0;
-        for (Frame f : frames) {
-            score += f.getScore();
-        }
-        return score;
-    }
-
-    private static List<Frame> determineFramesFormRolls(List<Integer> rolls) {
-        List<Frame> frames = new ArrayList<>();
-
-        Frame currentFrame = new Frame();
-        int currentRollIndex = -1;
-        for(int i = 0; i < MAX_FRAMES_PER_GAME; i++) {
-            currentRollIndex = getCurrentRollIndexAndAddToFrame(rolls, currentFrame, currentRollIndex);
-
-            if (currentFrame.isAStrike()) {
-                currentFrame.setNext2Rolls(rolls.get(currentRollIndex + 1), rolls.get(currentRollIndex + 2));
-                currentFrame = addAndResetFrame(frames, currentFrame);
-            } else {
-                currentRollIndex = getCurrentRollIndexAndAddToFrame(rolls, currentFrame, currentRollIndex);
-
-                if (currentFrame.isASpare()) {
-                    currentFrame.setNextRoll(rolls.get(currentRollIndex + 1));
-                }
-                currentFrame = addAndResetFrame(frames, currentFrame);
-            }
-        }
-
-        return frames;
-    }
-
-    private static int getCurrentRollIndexAndAddToFrame(List<Integer> rolls, Frame currentFrame, int currentRollIndex) {
-        currentRollIndex++;
-        currentFrame.addRoll(rolls.get(currentRollIndex));
-        return currentRollIndex;
-    }
-
-    private static Frame addAndResetFrame(List<Frame> frames, Frame currentFrame) {
-        frames.add(currentFrame);
-        return new Frame();
+        assertThat(BowlingScoreCalculator.calculateScore(NO_STRIKE_NOR_SPARE)).isEqualTo(90);
     }
 
     @Test
     public void shouldReturn10FramesFor12Strikes() {
-        List<Frame> frames = determineFramesFormRolls(ONLY_STRIKE);
+        List<BowlingScoreCalculator.Frame> frames = BowlingScoreCalculator.determineFramesFormRolls(ONLY_STRIKE);
         assertThat(frames.size()).isEqualTo(10);
         assertThat(frames.get(0).nextRoll).isEqualTo(10);
         assertThat(frames.get(0).nextNextRoll).isEqualTo(10);
@@ -100,7 +56,7 @@ public class BowlingScoreCalculatorTest {
 
     @Test
     public void shouldReturn10FramesFor12Spares() {
-        List<Frame> frames = determineFramesFormRolls(ONLY_SPARE);
+        List<BowlingScoreCalculator.Frame> frames = BowlingScoreCalculator.determineFramesFormRolls(ONLY_SPARE);
         assertThat(frames.size()).isEqualTo(10);
         assertThat(frames.get(0).nextRoll).isEqualTo(5);
         assertThat(frames.get(0).nextNextRoll).isEqualTo(0);
@@ -110,7 +66,7 @@ public class BowlingScoreCalculatorTest {
 
     @Test
     public void shouldReturn10FramesFor20Rolls() {
-        List<Frame> frames = determineFramesFormRolls(NO_STRIKE_NOR_SPARE);
+        List<BowlingScoreCalculator.Frame> frames = BowlingScoreCalculator.determineFramesFormRolls(NO_STRIKE_NOR_SPARE);
         assertThat(frames.size()).isEqualTo(10);
         assertThat(frames.get(0).getFirstRoll()).isEqualTo(4);
         assertThat(frames.get(0).getSecondRoll()).isEqualTo(5);
@@ -120,7 +76,7 @@ public class BowlingScoreCalculatorTest {
 
     @Test
     public void shouldCalculateTheScoreOfAStrikeWithTheNext2Rolls() {
-        Frame frame = new Frame();
+        BowlingScoreCalculator.Frame frame = new BowlingScoreCalculator.Frame();
         frame.addRoll(10);
         frame.nextRoll = 2;
         frame.nextNextRoll = 9;
@@ -129,50 +85,11 @@ public class BowlingScoreCalculatorTest {
 
     @Test
     public void shouldCalculateTheScoreOfASpareWithTheNextRoll() {
-        Frame frame = new Frame();
+        BowlingScoreCalculator.Frame frame = new BowlingScoreCalculator.Frame();
         frame.addRoll(5);
         frame.addRoll(5);
         frame.nextRoll = 2;
         assertThat(frame.getScore()).isEqualTo(12);
-    }
-
-    static class Frame {
-        List<Integer> rolls = new ArrayList<>(3);
-        int nextRoll = 0;
-        int nextNextRoll = 0;
-
-        private boolean isAStrike() {
-            return getFirstRoll() == 10;
-        }
-
-        private boolean isASpare() {
-            return getFirstRoll() + getSecondRoll() == 10;
-        }
-
-        int getFirstRoll() {
-            return rolls.get(0);
-        }
-
-        int getSecondRoll() {
-            return (rolls.size() < 2) ? 0 : rolls.get(1);
-        }
-
-        void addRoll(int roll) {
-            rolls.add(roll);
-        }
-
-        int getScore() {
-            return getFirstRoll() + getSecondRoll() + nextRoll + nextNextRoll;
-        }
-
-        private void setNext2Rolls(Integer nextRoll, Integer nextNextRoll) {
-            this.nextRoll = nextRoll;
-            this.nextNextRoll = nextNextRoll;
-        }
-
-        private void setNextRoll(Integer nextRoll) {
-            this.setNext2Rolls(nextRoll, 0);
-        }
     }
 
 }
