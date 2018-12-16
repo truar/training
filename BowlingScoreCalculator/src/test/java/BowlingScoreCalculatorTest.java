8220300b5b609800e4c445f11173802867dad28a
truar <thibault.ruaro@gmail.com>
2018-12-16 10:47:31 +0100

    Add a list to calculate the score of many rolls

--- BowlingScoreCalculator/src/test/java/BowlingScoreCalculatorTest.java
+++ BowlingScoreCalculator/src/test/java/BowlingScoreCalculatorTest.java
@@ -1,15 +1,20 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BowlingScoreCalculatorTest {
 
     @Test
-    public void shouldReturnTheRollScore() {
-        assertThat(8).isEqualTo(calculateScore(8));
+    public void shouldReturnTheSumOfTheRollsScore() {
+        List<Integer> rolls = new ArrayList<>();
+        rolls.add(8);
+        assertThat(8).isEqualTo(calculateScore(rolls));
     }
 
-    private int calculateScore(int roll) {
-        return roll;
+    private int calculateScore(List<Integer> rolls) {
+        return rolls.get(0);
     }
 }
