ff6fcd0bb1dd3f8f7f9018986f4134f37ad09ad5
truar <thibault.ruaro@gmail.com>
2018-12-16 11:14:11 +0100

    Added a new class Frame to group the roll

--- BowlingScoreCalculator/src/test/java/BowlingScoreCalculatorTest.java
+++ BowlingScoreCalculator/src/test/java/BowlingScoreCalculatorTest.java
@@ -100,4 +100,15 @@ public class BowlingScoreCalculatorTest {
         }
         return score;
     }
+
+    public static class Frame {
+        public int firstRoll;
+        public int secondRoll;
+
+        public Frame(int firstRoll, int secondRoll) {
+            this.firstRoll = firstRoll;
+            this.secondRoll = secondRoll;
+        }
+    }
+
 }
